8153a698e521af91c404d783325dcba041f5f04d
Isaac <isaac@zfamily.org>
2018-11-17 22:45:12 -0700

    made ml.py cleaner and ready for training.

--- ml.py
+++ ml.py
@@ -2,7 +2,6 @@ from sklearn.preprocessing import normalize
 from tensorflow import keras
 import tensorflow as tf
 import pickle as pk
-# import pandas as pd
 import numpy as np
 import my_numpy
 import codecs
@@ -11,16 +10,19 @@ import ujson
 import time
 import os
 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import StratifiedKFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-
 # cmd: > tensorboard --logdir=$PATH
+
+# save the current model
+def save_model(model):
+	keras.models.save_model(
+		model,
+		'{}/save/model.hdf5'.format(os.path.dirname(os.path.abspath(__file__)).replace('\\','/')),
+		overwrite=False,
+		include_optimizer=True
+	)
+
+
+# get and format all the data
 
 path = './data/'
 
@@ -37,9 +39,7 @@ for n, name in enumerate(files):
 	for i in range(len(data) - 1):
 		train_data.append(data[i])
 		lbls = data[i+1][:-6]
-		# train_lbls.append(lbls)
 		tmp.append(lbls[:int(len(lbls)/2)])
-	# break
 
 for turn in tmp:
 	t = []
@@ -57,30 +57,19 @@ for turn in tmp:
 train_data = np.array([np.array(xi) for xi in train_data])
 train_lbls = np.array([np.array(xi) for xi in train_lbls])
 
-# test_data = train_data[-20:]
-# test_lbls = train_lbls[-20:]
-# train_data = train_data[:-20]
-# train_lbls = train_lbls[:-20]
-
 train_data = normalize(train_data)
 # train_lbls = normalize(train_lbls)
 
-# for l in train_lbls[2]:
-# 	print (l)
+# at this point data is fully formatted, now to train the model
 
 print (train_data.shape)
 print (train_lbls.shape)
 
-# print (yay)
 model = keras.Sequential([
 	keras.layers.Dense(500, input_shape=(train_data.shape[1],)),
-	# keras.layers.BatchNormalization(),
 	keras.layers.Dense(600, activation=tf.nn.sigmoid),
-	# keras.layers.BatchNormalization(),
 	keras.layers.Dense(700, activation=tf.nn.sigmoid),
-	# keras.layers.BatchNormalization(),
 	keras.layers.Dense(train_lbls.shape[1], activation=tf.nn.sigmoid),
-	# keras.layers.BatchNormalization()
 ])
 
 
@@ -104,11 +93,18 @@ lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2, patience=10)
 cb_list = [tb, lr]
 
 model.fit(train_data, train_lbls, epochs=5, callbacks=cb_list)
+save_model(model)
 
 dim_x = len(model.get_layer(index=0).get_weights()[0])
 dim_y = len(model.get_layer(index=0).get_weights()[0][0])
-print (dim_x, dim_y)
 
+
+
+
+
+
+
+# here and below is all saving/loading the weights
 
 file_path = "weights/model"
 indicies = [0,1,2,3]
@@ -134,13 +130,6 @@ print ('Loading time: {}'.format(elapsed_time))
 
 x = [y%10 for y in range(dim_x)]
 
-print (len(x))
-print (len(weights[0]), len(weights[0][0]))
-print (len(weights[1]), len(weights[1][0]))
-print (len(weights[2]), len(weights[2][0]))
-print (len(weights[3]), len(weights[3][0]))
-print ()
-
 def forward_prop(input, indicies, weights, biases):
 	o = my_numpy.add_vecs(my_numpy.mult_vec(weights[0],x), biases[0])
 	for i in indicies[1:]:
@@ -153,17 +142,5 @@ start_time = time.time()
 
 o = forward_prop(x, indicies, weights, biases)
 
-# l1 = my_numpy.add_vecs(my_numpy.mult_vec(weights[0],x), biases[0])
-# l2 = my_numpy.add_vecs(my_numpy.mult_vec(weights[1],l1), biases[1])
-# l3 = my_numpy.add_vecs(my_numpy.mult_vec(weights[2],l2), biases[2])
-# o = my_numpy.sigmoid(l3)
-
 elapsed_time = time.time() - start_time
 print ('\nMatrix Operations Time: {}'.format(elapsed_time))
-
-# print (len(l3))
-# for l in enumerate(o):
-# 	if l > .98:
-# 		print (l)
-
-print (len(o))
