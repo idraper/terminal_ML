6c63cc01ba09c730c4baf605dd256d0952425bad
Isaac <isaac@zfamily.org>
2018-11-18 01:22:33 -0700

    modifications

--- ml.py
+++ ml.py
@@ -67,10 +67,16 @@ print (train_lbls.shape)
 
 model = keras.Sequential([
 	keras.layers.Dense(500, input_shape=(train_data.shape[1],)),
+	keras.layers.BatchNormalization(),
 	keras.layers.Dense(600, activation=tf.nn.sigmoid),
+	keras.layers.BatchNormalization(),
 	keras.layers.Dense(700, activation=tf.nn.sigmoid),
+	keras.layers.BatchNormalization(),
 	keras.layers.Dense(train_lbls.shape[1], activation=tf.nn.sigmoid),
+	keras.layers.BatchNormalization(),
 ])
+indicies = [0,2,4,6]
+# indicies = [0,1,2,3]
 
 
 # RMSprop lr=.001
@@ -79,21 +85,21 @@ model = keras.Sequential([
 # NAdam lr=.005
 
 model.compile(optimizer=keras.optimizers.RMSprop(lr=.001), 
-			  loss='binary_crossentropy',
+			  loss='mse',
 			  metrics=['accuracy'])
 
 tb = keras.callbacks.TensorBoard(log_dir='./ML_summaries/', histogram_freq= 0, write_graph=True, write_images=True)
 
 save = keras.callbacks.ModelCheckpoint('model.hdf5', monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
 
-stop = keras.callbacks.EarlyStopping(monitor='acc', min_delta=0.001)
+stop = keras.callbacks.EarlyStopping(monitor='acc', min_delta=0, patience=2)
 
 lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2, patience=10)
 
-cb_list = [tb, lr]
+cb_list = [tb, lr, stop]
 
-model.fit(train_data, train_lbls, epochs=500, callbacks=cb_list)
-save_model(model, 'KeKroepes_3.27')
+model.fit(train_data, train_lbls, epochs=100, callbacks=cb_list)
+save_model(model, 'KeKroepes_3.27_0.1')
 
 dim_x = len(model.get_layer(index=0).get_weights()[0])
 dim_y = len(model.get_layer(index=0).get_weights()[0][0])
@@ -107,10 +113,9 @@ dim_y = len(model.get_layer(index=0).get_weights()[0][0])
 # here and below is all saving/loading the weights
 
 file_path = "weights/model"
-indicies = [0,1,2,3]
-for i in indicies:
-	json.dump(model.get_layer(index=i).get_weights()[0].tolist(), codecs.open('{}_w{}.json'.format(file_path, i+1), 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
-	json.dump(model.get_layer(index=i).get_weights()[1].tolist(), codecs.open('{}_b{}.json'.format(file_path, i+1), 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+for i,pos in enumerate(indicies):
+	json.dump(model.get_layer(index=pos).get_weights()[0].tolist(), codecs.open('{}_w{}.json'.format(file_path, i+1), 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+	json.dump(model.get_layer(index=pos).get_weights()[1].tolist(), codecs.open('{}_b{}.json'.format(file_path, i+1), 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
 
 
 weights = []
@@ -119,7 +124,7 @@ biases = []
 print ("\nLoading...")
 start_time = time.time()
 
-for i in indicies:
+for i,_ in enumerate(indicies):
 	with open('{}_w{}.json'.format(file_path, i+1)) as file:
 		weights.append(ujson.load(file))
 	with open('{}_b{}.json'.format(file_path, i+1)) as file:
@@ -132,8 +137,8 @@ x = [y%10 for y in range(dim_x)]
 
 def forward_prop(input, indicies, weights, biases):
 	o = my_numpy.add_vecs(my_numpy.mult_vec(weights[0],x), biases[0])
-	for i in indicies[1:]:
-		o = my_numpy.add_vecs(my_numpy.mult_vec(weights[i],o), biases[i])
+	for i,_ in enumerate(indicies[1:]):
+		o = my_numpy.add_vecs(my_numpy.mult_vec(weights[i+1],o), biases[i+1])
 	o = my_numpy.sigmoid(o)
 	return o
 
